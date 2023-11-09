1e921185f45d23d5520926876dc7d9c91db85080
Arkestrat <115097705+Arkestrat@users.noreply.github.com>
2023-11-09 23:35:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//страница 16 номер 22
+Console.WriteLine("Введите координату x");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату y");
+double y = Convert.ToDouble(Console.ReadLine());
+if (y >= 0 && x >= 4 && y <= -6 - x || y <= 0 && x <= -4 && y >= 0 - x)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
